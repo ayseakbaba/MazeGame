221b9dd361508260b6dbf1b5a22e7dcc81601662
ayseakbaba <ayse.akbaba@detaysoft.com>
2025-05-14 22:02:29 +0300

    Add project files.

---
+++ MazeGame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MazeGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
